fbaca683794b33ec108f7e72ca60b4325d62b825
mikehwagz <hmiklw@me.com>
2017-03-19 20:50:39 -0400

    work in progress

--- src/js/sections/home.js
+++ src/js/sections/home.js
@@ -2,6 +2,7 @@ import config from 'config'
 import utils from 'utils'
 import classes from 'dom-classes'
 import Default from './default'
+import 'pixi.js'
 
 class Home extends Default {
 
@@ -22,7 +23,48 @@ class Home extends Default {
 
 		super.ready()
 
+		this.renderer = new PIXI.autoDetectRenderer(config.width, config.height, { transparent: true })
+		this.renderer.view.style.position = 'absolute'
+		this.renderer.view.style.display = 'block'
+		this.renderer.view.style.pointerEvents = 'none'
+		this.renderer.autoResize = true;
+
+		config.body.appendChild(this.renderer.view)
+
+		this.stage = new PIXI.Container()
+
+		PIXI.loader
+		  .add('assets/img/sprite-01.png')
+		  .load(this.setupSprite)
+
 		done()
+	}
+
+	setupSprite() {
+
+		console.log('entered setup')
+	  // //Create the `tileset` sprite from the texture
+	  // const texture = PIXI.utils.TextureCache['assets/img/sprite-01.png']
+		//
+	  // // Create a rectangle object that defines the position and
+	  // // size of the sub-image you want to extract from the texture
+	  // const rectangle = new PIXI.Rectangle(192, 128, 64, 64)
+		//
+	  // // Tell the texture to use that rectangular section
+	  // texture.frame = rectangle
+		//
+	  // // Create the sprite from the texture
+	  // const sprite = new PIXI.Sprite(texture)
+		//
+	  // // Position the rocket sprite on the canvas
+	  // sprite.x = 0
+	  // sprite.y = 0
+		//
+	  // // Add the rocket to the stage
+	  // this.stage.addChild(sprite)
+		//
+	  // // Render the stage
+	  // this.renderer.render(stage)
 	}
 
 	animateIn(req, done) {
@@ -45,6 +87,13 @@ class Home extends Default {
 			ease: Expo.easeInOut,
 			onComplete: done
 		})
+	}
+
+	resize(width, height) {
+
+		super.resize()
+
+		this.renderer.resize(width, height)
 	}
 
 	destroy(req, done) {
