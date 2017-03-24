8f847f96d43f48d34fce8dccac10e31b95547a06
mikehwagz <hmiklw@me.com>
2017-03-23 22:01:17 -0400

    add catering request post type, increase width of wp admin menu and replace howdy message with welcome

--- src/functions.php
+++ src/functions.php
@@ -30,7 +30,22 @@ class PonoOno extends TimberSite {
 	}
 
 	function register_post_types() {
-		//this is where you can register custom post types
+		$post_labels = array(
+			'name' 			    	 	 => 'Catering Requests',
+			'singular_name' 		 => 'Catering Request',
+			'add_new' 		    	 => 'Add New',
+			'add_new_item'  		 => 'Add New Request',
+			'edit'		        	 => 'Edit',
+			'edit_item'	      	 => 'Edit Request',
+			'new_item'	         => 'New Request',
+			'view' 			         => 'View Request',
+			'view_item' 		     => 'View Request',
+			'search_term'   	   => 'Search Requests',
+			'parent' 		    		 => 'Parent Catering Request',
+			'not_found' 				 => 'No Catering Requests found',
+			'not_found_in_trash' => 'No Catering Requests in Trash'
+		);
+		register_post_type( 'catering_request', array( 'labels' => $post_labels, 'public' => true ) );
 	}
 
 	function register_taxonomies() {
@@ -38,7 +53,7 @@ class PonoOno extends TimberSite {
 	}
 
 	function add_to_context( $context ) {
-		
+
 		$context['menu'] = new TimberMenu();
 		$context['site'] = $this;
 
@@ -54,3 +69,33 @@ class PonoOno extends TimberSite {
 	}
 }
 new PonoOno();
+
+
+
+/* WP Admin Customizations */
+
+// Increase width of wp admin menu to fit custom post type labels
+function custom_admin_style() {
+	echo '<style>
+	#adminmenu,
+	#adminmenu .wp-submenu,
+	#adminmenuback,
+	#adminmenuwrap {
+		width: 200px;
+	}
+	#wpcontent, #wpfooter {
+    margin-left: 200px;
+	}
+	#adminmenu .wp-submenu {
+		left: 200px;
+	}
+	</style>';
+}
+add_action('admin_head', 'custom_admin_style');
+
+// Change Howdy message to Welcome
+function howdy_message($translated_text, $text, $domain) {
+    $new_message = str_replace('Howdy', 'Welcome', $text);
+    return $new_message;
+}
+add_filter('gettext', 'howdy_message', 10, 3);
